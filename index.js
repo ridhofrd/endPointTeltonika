0d87da71c99223de0a6df99c0a9d1f68a0578e06
Lukmannul Hakim Firdaus <elhakimfirdaus@gmail.com>
2024-10-16 10:16:35 +0700

    add param imei

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 
 app.get('/get_json_string', async(req,res) => {
     try{
+        const imei = req.query.imei
         const codec_data = req.query.codec_data
         const io_data = req.query.io_data
 
-        res.status(200).send({ codec_data, io_data })
+        res.status(200).send({ imei, codec_data, io_data })
+        console.log(imei)
         console.log(codec_data)
         console.log(io_data)
     }
