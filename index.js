49b8c90788e70a522e50120fd0736bda6cb9ac3d
Lukmannul Hakim Firdaus <elhakimfirdaus@gmail.com>
2024-10-19 19:55:28 +0700

    add endpoint save_data_loc

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
 
 const app = express();
+
+app.use(bodyParser.json()); 
 
 app.get('/get_json_string', async(req,res) => {
     try{
@@ -18,6 +21,18 @@ app.get('/get_json_string', async(req,res) => {
         res.status(500).send("Server Error")
     }
 })
+
+app.post('/save_data_loc', (req, res) => {
+    
+    const imei = req.body.imei
+    const codec_data = req.body.codec_data
+    const io_data = req.body.io_data
+
+    res.status(200).send({ imei, codec_data, io_data})
+    console.log(imei)
+    console.log(codec_data)
+    console.log(io_data)
+});
 
 app.listen(3000)
 
