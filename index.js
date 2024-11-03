0008cdaf7705eb35e4d75f1a555008f10c18073d
Lukmannul Hakim Firdaus <elhakimfirdaus@gmail.com>
2024-11-03 07:52:58 +0700

    call api save db data teltonika

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const axios = require('axios');
+
 
 const app = express();
 
@@ -10,7 +12,7 @@ app.get('/get_json_string', async(req,res) => {
         const imei = req.query.imei
         const codec_data = req.query.codec_data
         const io_data = req.query.io_data
-
+        
         res.status(200).send({ imei, codec_data, io_data })
         console.log(imei)
         console.log(codec_data)
@@ -22,16 +24,37 @@ app.get('/get_json_string', async(req,res) => {
     }
 })
 
-app.post('/save_data_loc', (req, res) => {
+app.post('/save_data_loc', async (req, res) => {
     
     const imei = req.body.imei
     const codec_data = req.body.codec_data
     const io_data = req.body.io_data
 
-    res.status(200).send({ imei, codec_data, io_data})
-    console.log(imei)
-    console.log(codec_data)
-    console.log(io_data)
+    try {
+        // Mengirim data ke API eksternal menggunakan axios
+        const response = await axios.post('https://smart-coldchain.com/api/teltonikaDB', {
+            imei,
+            codec_data,
+            io_data
+        });
+
+        // Mengirimkan respons dari API eksternal kembali ke client
+        res.status(response.status).send(response.data);
+
+        // Logging data
+        console.log('IMEI:', imei);
+        console.log('Codec Data:', codec_data);
+        console.log('IO Data:', io_data);
+    } catch (error) {
+        // Menghandle error dari permintaan API
+        console.error('Error posting data to external API:', error.message);
+        res.status(500).send({ error: 'Failed to post data to external API' });
+    }
+    
+    // res.status(200).send({ imei, codec_data, io_data})
+    // console.log(imei)
+    // console.log(codec_data)
+    // console.log(io_data)
 });
 
 app.listen(3000)
